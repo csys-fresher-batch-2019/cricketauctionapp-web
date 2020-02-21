1fdf4b9cdb555aa4da98399bd9d84e4aae43dbec
Sundaramoorthi R <sundara2803@gmail.com>
2020-02-21 13:00:36 +0530

    Added clean scripts

---
+++ cricketauctionapp-core/src/main/resources/clean.sql
@@ -0,0 +1,12 @@
+drop table career;
+drop table cricketing;
+drop table players;
+drop table country;
+drop table teamplayer;
+drop table team;
+
+drop sequence player_id_sq;
+drop sequence cric_no_sq;
+drop sequence career_no_sq;
+drop sequence playerr_id_sq;
+drop sequence team_id_sq;
